8515f402a38c808549f4e6a3cf8735177644c890
nathan <kvngnathan8420@gmail.com>
2023-05-24 21:49:08 +0000

    Render product list

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -18,6 +19,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("shop:product_list_by_category", args=[self.slug])
+    
 
 
 class Product(models.Model):
@@ -42,3 +47,6 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse("shop:product_detail", args=[self.id , self.slug])
+    
--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path("/", views.product_list, name="produce_list")
+    path('', views.product_list, name="product_list"),
+    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+
 ]
 
