b73d6945032dbbaf02d73c6f724eaae34a242af8
nathan <kvngnathan8420@gmail.com>
2023-05-24 16:45:31 +0000

    Create Template views, add static files

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def product_list(request):
+
+    return render(request, "shop/product/list.html")
+
+
+def product_detail(request):
+
+    return render(request, "shop/product/detail.html")   
