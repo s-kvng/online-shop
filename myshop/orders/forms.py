a712296f960a3bfdbb155726cd65c3ce9e176585
nathan <kvngnathan8420@gmail.com>
2023-06-06 04:43:21 +0000

    Order items feature/ create form to save order model

--- myshop/orders/forms.py
+++ myshop/orders/forms.py
@@ -3,6 +3,18 @@ from .models import Order
 
 
 class OrderCreateForm(forms.ModelForm):
-    model = Order
-    fields = ['first_name', 'last_name', 'email' , 
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'email' , 
               'address', 'postal_code', 'city']
+    
+        widgets = {
+
+            'first_name': forms.TextInput(attrs={'class':'form-control'}),
+            'last_name': forms.TextInput(attrs={'class':'form-control'}),
+            'email': forms.EmailInput(attrs={'class':'form-control'}),
+            'address': forms.TextInput(attrs={'class':'form-control'}),
+            'postal_code': forms.TextInput(attrs={'class':'form-control'}),
+            'city': forms.TextInput(attrs={'class':'form-control'}),
+        
+        }
